58fa8cb79e8da4696c5600d3f5199e207ca9846d
LiubomyrFadieiev <61575710+LiubomyrFadieiev@users.noreply.github.com>
2020-05-22 12:53:41 +0300

    Update main.cpp

--- CW3/Task1/main.cpp
+++ CW3/Task1/main.cpp
@@ -1,58 +1,29 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "struct.h"
 using namespace std;
 
-void take(string* a, int &k, string text){
-	ifstream out;
-	out.open(text);
-	string line;
-	bool repeat, take;
-	if(out.is_open()){
-		while(getline(out,line)){
-			repeat = false; take = false;
-			for (int i = 0; i<k; i++){
-				if(line==a[i]){
-					a[i] = "";
-					repeat = true;
-				}
-			}
-			if (!repeat){
-			for (int i = 0; i<k; i++){
-				if(a[i]==""){
-					a[i] = line;
-					take = true;
-				}
-			}
-			}
-			if(!repeat && !take){
-				a[k++] = line;
-			}
-		}
-	}
-}
 
 int main() {
-	string* a = new string[20]; 
+	int n = 5;
+	struct Auto* a = new struct Auto[n];
 	string line;
 	ifstream out;
-	out.open("in1.txt");
-	int k = 0;
+	out.open("in.txt");
+	int i = 0;
 	if(out.is_open()){
-		while(getline(out,line)){
-			a[k++] = line;
+		while(out){
+			getline(out,line,',');
+			a[i].mark = line;
+			getline(out,line,',');
+			a[i].model = line;
+			getline(out,line,',');
+			a[i].year = line;
+			getline(out,line,',');
+			a[i].engine = line;
+			i++;
 		}
 	}
-	out.close();
-	string b[2] = {"in2.txt","in3.txt"};
-	take(a,k,b[0]);
-	take(a,k,b[1]);
-	ofstream inr;
-	inr.open("out.txt");
-	for (int i = 0; i<k; i++){
-		if(a[i]!=""){
-			inr << a[i] << "\n";
-		}
-	}
-	delete[] a;
+	sorted(a,n);
 }
